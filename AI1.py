beea609d524130b8239da88802fd191597fe5c2f
SiyonaAgarwa <113793806+SiyonaAgarwa@users.noreply.github.com>
2022-09-17 23:30:41 +0530

    Add files via upload

---
+++ AI1.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+k=[]
+l=[]
+for i in list1:
+    if i>0:
+        
+        k.append(i)
+for j in list2:
+    if j>0:
+        l.append(j)
+        
+print(k)
+print(l)
